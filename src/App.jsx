2c7e3c38e3150e1f85b5d524971ec9d03c24bec3
Yash Hegde <yash4success@gmail.com>
2024-01-01 13:11:02 +0530

    socials added

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,22 @@ function App() {
       <h1>Resume Generator for NIT Rourkela</h1>
       <img src={resumelogo} className="resume-logo" alt="resume logo"/>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>Made by <code>Yash Hegde </code>@NIT Rourkela</p>
+
+        Get in touch with me on <br>
+        </br><a href="https://www.linkedin.com/in/yash-hegde-927721201/">LinkedIn</a>
+        <br>
+        </br>
+        <a href="https://twitter.com/YashHegde7">Twitter</a>
+        <br>
+        </br>
+        {/* onclick: redirect to email at yash4success@gmail.com */}
+        <a href="mailto:yash4success@gmail.com">Gmail</a>
+        <br>
+        </br>
+         <a href="https://github.com/Yh010">Github</a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
